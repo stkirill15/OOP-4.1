d48eda6caa1abad2188fc1e8875bc5ef5f07defe
Kirill <Kirill@MEGA79>
2023-05-23 16:21:12 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace OOP_4._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 69);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(45, 19);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Ctrl";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(12, 94);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(182, 19);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "Выделение по одному кругу";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 128);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Delete";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_4._1
+{
+    
+    public partial class Form1 : Form
+    {
+        private List<CCircle> Circles = new List<CCircle>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (checkBox1.Checked == false)//не нажат ctrl
+            {
+                foreach (CCircle list_Circle in Circles)
+                {
+                    list_Circle.set_color("Black"); //снимает выделение со всех объектов
+                }
+                CCircle Circle = new CCircle(e.X, e.Y, 20);//создает новый объект с выделением
+                Circles.Add(Circle);
+            }
+            if (checkBox1.Checked == true) //нажат ctrl
+            {
+                foreach (CCircle list_Circle in Circles)
+                {
+                    if (list_Circle.checkCircle(e) == true && checkBox2.Checked == true) //проверка на массо-вое выделение
+                    {
+                        break;
+                    }
+                }
+                Refresh();
+            }
+            Refresh();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (Control.ModifierKeys == Keys.Control)
+            {
+                checkBox1.Checked = true;
+                CCircle.set_ctrl(true);
+            }
+        }
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            checkBox1.Checked = false;
+            CCircle.set_ctrl(false);
+        }
+        public class CCircle
+        {
+            private int x, y;
+            private float radius;
+            private string color = "Red";
+            private static bool check_ctrl = false;
+            public CCircle(int x, int y, float radius)
+            {
+                this.x = x;
+                this.y = y;
+                this.radius = radius;
+            }
+            public void draw(Graphics Canvas)
+            {
+                if (color == "Red")
+                {
+                    Canvas.DrawEllipse(new Pen(Color.Red), x - radius, y - radius, radius * 2, radius * 2);
+                }
+                else
+                {
+                    Canvas.DrawEllipse(new Pen(Color.Black), x - radius, y - radius, radius * 2, radius * 2);
+                }
+            }
+            public void set_color(string color)
+            {
+                this.color = color;
+            }
+            public string get_color()
+            {
+                return color;
+            }
+            public bool checkCircle(MouseEventArgs e)
+            {
+                if (check_ctrl)
+                {
+                    if (Math.Pow(e.X - x, 2) + Math.Pow(e.Y - y, 2) <= Math.Pow(radius, 2) && color != "Red")
+                    {
+                        color = "Red";
+                        return true;
+                    }
+                }
+                return false;
+            }
+            static public void set_ctrl(bool check) //сеттер флага выделения
+            {
+                check_ctrl = check;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < Circles.Count(); i++)
+            {
+                if (Circles[i].get_color() == "Red") //проверка выделения объектов
+                {
+                    Circles.RemoveAt(i); //удаление выделенных объектов
+                    --i;
+                }
+            }
+            Refresh();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (CCircle list_Circle in Circles)
+            {
+                list_Circle.draw(e.Graphics); //Рисует все круги из списка
+            }
+
+        }
+    }
+}
